445b4c2e119d8b1913b1c88a421b523f3f354126
Aяad Ashkan <65897590+AradAshkan@users.noreply.github.com>
2025-06-14 18:50:05 +0330

    Add files via upload

---
+++ Arad_Loading/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Arad | github.com/aradashkan'
+description 'QuantomRP Loading Screen'
+version '1.0.0'
+
+loadscreen 'html/index.html'
+loadscreen_cursor 'yes'
+
+files {
+    'html/**/*.*'
+}
